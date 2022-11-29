a6e19030ef411c72ffaf6ca986620a07adb72b25
Poina <polinka77789@mail.ru>
2022-11-29 18:08:32 +0300

    Добавлен бот

---
+++ AIOBot/create_bot.py
@@ -0,0 +1,4 @@
+from aiogram import Bot, Dispatcher
+
+bot = Bot(token='5836651104:AAFrSyXpTbOqGoi_FPG1JTMmzqkBneOALII')
+dp = Dispatcher(bot)
---
+++ AIOBot/handlers.py
@@ -0,0 +1,9 @@
+from aiogram import types, Dispatcher
+
+import commands
+
+
+def registret_handlers(dp: Dispatcher):
+    dp.register_message_handler(commands.start, commands=['start'])
+    dp.register_message_handler(commands.stop, commands=['stop'])
+    dp.register_message_handler(commands.anything)
